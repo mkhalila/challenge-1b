71b73a01953a8a876e88601f530b7becf530cac0
Khalil Ahmed <mkhalila@outlook.com>
2016-10-06 16:17:33 +0100

    Created member variables and constructor for Item

--- Item.h
+++ Item.h
@@ -1,7 +1,27 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include <string>
+#include <iostream>
+using std::string;
+
+using std::cout;
+using std::endl;
+
 // TODO: your code goes here
+class Item {
+private:
+	double latitude;
+	double longitude;
+	string id;
+	int time;
+
+public: 
+	Item(const double latIn, const double longIn, const string idIn, const int timeIn) 
+	: latitude(latIn), longitude(longIn), id(idIn), time(timeIn) {
+
+	}
+};
 
 
 
